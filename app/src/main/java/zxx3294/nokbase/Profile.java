0069a82b7629fff2d3310b5183bc245fe54e690a
Alan19 <Alan19@users.noreply.github.com>
2017-12-03 19:26:31 -0500

    Changed color scheme, added navigation

--- app/src/main/java/zxx3294/nokbase/Profile.java
+++ app/src/main/java/zxx3294/nokbase/Profile.java
@@ -1,7 +1,9 @@
 package zxx3294.nokbase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    protected void goToMain(View view){
+        Intent intent = new Intent(Profile.this, MainActivity.class);
+        startActivity(intent);
     }
 }
